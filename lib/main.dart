e219f4be7af26a193047fbffbed324b2eac4ca1e
dia1836 <dia1836@hotmail.co.jp>
2024-12-22 14:58:24 +0900

    タイトル修正

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: '成長合成シミュレーター',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
         useMaterial3: true,
